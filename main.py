e98b56e4c14ebb5a83482fdd47adf3ad2ee0214f
Jose Cerrejon <ulysess@gmail.com>
2024-11-14 12:38:12 +0100

    feat: Add CORS middleware to allow requests from specified origins

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
 
-# Health API Endpoint
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/health")
 async def server_health():
     return {"status": "OK", "message": "Server is up and running."}
