dfe1fda34330418deb9a522b93e31dce73496c16
Jose Cerrejon <ulysess@gmail.com>
2024-11-17 17:56:42 +0100

    feat: Refactor task retrieval methods and enhance error handling in task creation

--- src/tasks/controllers.py
+++ src/tasks/controllers.py
@@ -18,7 +18,7 @@ def get_task_list(session: Session):
 
 
 def get_task(id: int, session: Session):
-    task = session.query(models.Task).get(id)
+    task = session.get(models.Task, id)
     if task is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Task with id {id} not found"
@@ -27,15 +27,21 @@ def get_task(id: int, session: Session):
 
 
 def create_task(task: schemas.TaskCreate, session: Session):
-    task_db = models.Task(task=task.task)
-    session.add(task_db)
-    session.commit()
-    session.refresh(task_db)
-    return schemas.OkResponse(**task_db.__dict__)
+    try:
+        task_db = models.Task(task=task.task)
+        session.add(task_db)
+        session.commit()
+        session.refresh(task_db)
+        return schemas.OkResponse(**task_db.__dict__)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error creating task: {str(e)}",
+        )
 
 
 def update_task(id: int, task: str, session: Session):
-    task_db = session.query(models.Task).get(id)
+    task_db = session.get(models.Task, id)
     if task_db:
         task_db.task = task
         session.commit()
@@ -45,7 +51,7 @@ def update_task(id: int, task: str, session: Session):
 
 
 def delete_task(id: int, session: Session):
-    task_db = session.query(models.Task).get(id)
+    task_db = session.get(models.Task, id)
     if not task_db:
         raise HTTPException(status_code=404, detail=f"Task with id {id} not found")
     session.delete(task_db)
